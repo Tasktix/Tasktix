bf3ffd8d42d7d7baf0b9d2783acb0739883329c1
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-11-09 04:46:23 +0000

    Add unique database indicies

--- prisma/migrations/20251109044725_init/migration.sql
+++ prisma/migrations/20251109044725_init/migration.sql
@@ -8,6 +8,8 @@ CREATE TABLE `User` (
     `dateCreated` DATETIME(0) NOT NULL,
     `dateSignedIn` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
+    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -63,6 +65,7 @@ CREATE TABLE `Item` (
 
     INDEX `Item_sectionId_idx`(`sectionId`),
     INDEX `Item_parentId_idx`(`parentId`),
+    UNIQUE INDEX `Item_sectionId_parentId_sectionIndex_key`(`sectionId`, `parentId`, `sectionIndex`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
