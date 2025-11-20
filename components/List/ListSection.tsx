79145a8031137c4ee44f91a887f8f405b0219e3f
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-11-19 19:16:24 -0800

    Fix unintuitive variable scope

--- components/List/ListSection.tsx
+++ components/List/ListSection.tsx
@@ -367,7 +367,7 @@ function compareFilter(
         value instanceof Date &&
         value.getTime() > item.dateCompleted.getTime()
       );
-    case 'completedOn':
+    case 'completedOn': {
       if (!(value instanceof Date)) return false;
 
       const start = structuredClone(value);
@@ -384,6 +384,7 @@ function compareFilter(
         start.getTime() <= item.dateCompleted.getTime() &&
         end.getTime() > item.dateCompleted.getTime()
       );
+    }
     case 'completedAfter':
       if (value instanceof Date) value.setHours(23, 59, 59, 999);
 
