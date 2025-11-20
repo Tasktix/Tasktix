6de2e90d603544eaec980225b0cfdddf083f9725
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-11-19 23:17:14 -0800

    Fix unnecessary negation

--- components/TimeInput.tsx
+++ components/TimeInput.tsx
@@ -80,7 +80,7 @@ export default function TimeInput({
       labelPlacement={labelPlacement}
       size={size}
       tabIndex={tabIndex}
-      value={value !== undefined ? formatTime(value) : time}
+      value={value === undefined ? time : formatTime(value)}
       variant={variant}
       onValueChange={updateTime}
     />
