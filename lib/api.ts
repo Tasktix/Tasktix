e45f3f101227e7802103d463ffa0d0a78159f234
lilyvesstible <135365164+lilyvesstible@users.noreply.github.com>
2025-12-03 20:07:23 -0800

    Added Documentation 2

--- lib/api.ts
+++ lib/api.ts
@@ -23,10 +23,14 @@ export interface ServerResponse {
 }
 
 export default {
+  // TS requires `this: void` to allow referencing `api.get` in tests without binding `this`.
+  // DeepSource is unfamiliar with this use of `void` in a type signature - skipcq: JS-0333
   get(this: void, resource: string) {
     return request(resource, 'GET');
   },
 
+  // TS requires `this: void` to allow referencing `api.post` in tests without binding `this`.
+  // DeepSource is unfamiliar with this use of `void` in a type signature - skipcq: JS-0333
   post(
     this: void,
     resource: string,
@@ -36,6 +40,8 @@ export default {
     return request(resource, 'POST', data, encodingType);
   },
 
+  // TS requires `this: void` to allow referencing `api.put` in tests without binding `this`.
+  // DeepSource is unfamiliar with this use of `void` in a type signature - skipcq: JS-0333
   put(
     this: void,
     resource: string,
@@ -45,6 +51,8 @@ export default {
     return request(resource, 'PUT', data, encodingType);
   },
 
+  // TS requires `this: void` to allow referencing `api.patch` in tests without binding `this`.
+  // DeepSource is unfamiliar with this use of `void` in a type signature - skipcq: JS-0333
   patch(
     this: void,
     resource: string,
@@ -54,6 +62,8 @@ export default {
     return request(resource, 'PATCH', data, encodingType);
   },
 
+  // TS requires `this: void` to allow referencing `api.delete` in tests without binding `this`.
+  // DeepSource is unfamiliar with this use of `void` in a type signature - skipcq: JS-0333
   delete(this: void, resource: string) {
     return request(resource, 'DELETE');
   }
