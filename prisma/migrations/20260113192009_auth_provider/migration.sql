ca6542fc2a87bbff300d8fddfaab8e04d04c767e
Ryan Grossman <ryangman423@gmail.com>
2026-01-23 10:11:26 -0800

    Fix databse userID foreign key lengths for auth-provider

--- prisma/migrations/20260113192009_auth_provider/migration.sql
+++ prisma/migrations/20260113192009_auth_provider/migration.sql
@@ -114,3 +114,25 @@ ALTER TABLE `ItemAssignee` ADD CONSTRAINT `ItemAssignee_userId_fkey` FOREIGN KEY
 
 -- AddForeignKey
 ALTER TABLE `Account` ADD CONSTRAINT `Account_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- DropForeignKey
+ALTER TABLE `ItemAssignee` DROP FOREIGN KEY `ItemAssignee_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `ListMember` DROP FOREIGN KEY `ListMember_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `ItemAssignee` DROP PRIMARY KEY,
+    MODIFY `userId` CHAR(191) NOT NULL,
+    ADD PRIMARY KEY (`userId`, `itemId`);
+
+-- AlterTable
+ALTER TABLE `ListMember` DROP PRIMARY KEY,
+    MODIFY `userId` CHAR(191) NOT NULL,
+    ADD PRIMARY KEY (`userId`, `listId`);
+
+-- AddForeignKey
+ALTER TABLE `ListMember` ADD CONSTRAINT `ListMember_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ItemAssignee` ADD CONSTRAINT `ItemAssignee_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
