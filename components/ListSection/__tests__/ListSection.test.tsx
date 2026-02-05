92732053cd71384de9ab85dad70b695e777072ad
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2026-02-05 13:50:48 -0800

    Try fixing tests

--- components/ListSection/__tests__/ListSection.test.tsx
+++ components/ListSection/__tests__/ListSection.test.tsx
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * @jest-environment jsdom
+ * @vitest-environment jsdom
  */
 
 import '@testing-library/jest-dom';
