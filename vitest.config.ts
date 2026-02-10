be8e780c82fb9e3c266ae35a818acdeaa0cbbb83
sabri abounozha <abounozs@oregonstate.edu>
2026-02-10 12:37:14 -0800

    Updates

--- vitest.config.ts
+++ vitest.config.ts
@@ -19,6 +19,10 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
