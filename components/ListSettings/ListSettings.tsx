55fa4690da5656e6791b623923c18c327fcfe2be
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2026-02-18 23:30:58 -0800

    Move ListAction dispatches to SSE instead of after API calls resolve

--- components/ListSettings/ListSettings.tsx
+++ components/ListSettings/ListSettings.tsx
@@ -112,7 +112,6 @@ export default function ListSettings({
                 title='General'
               >
                 <GeneralSettings
-                  dispatchList={dispatchList}
                   hasDueDates={hasDueDates}
                   hasTimeTracking={hasTimeTracking}
                   isAutoOrdered={isAutoOrdered}
