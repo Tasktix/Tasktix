5b514897ffd43ffc0f3b2314be0dbec231ee08dc
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2026-03-15 14:56:04 -0700

    feat: allow members to be removed from a list

--- components/ListSettings/MemberSettings.tsx
+++ components/ListSettings/MemberSettings.tsx
@@ -22,9 +22,10 @@ import {
   Select,
   Selection,
   SelectItem,
+  useDisclosure,
   User
 } from '@heroui/react';
-import { SendPlus } from 'react-bootstrap-icons';
+import { PersonPlusFill, PersonXFill } from 'react-bootstrap-icons';
 import { FormEvent, useState } from 'react';
 
 import { getBackgroundColor } from '@/lib/color';
@@ -33,6 +34,8 @@ import MemberRole from '@/lib/model/memberRole';
 import api from '@/lib/api';
 import { sortRolesByPermissions } from '@/lib/sort';
 import { addToastForError } from '@/lib/error';
+
+import ConfirmModal from '../ConfirmModal';
 
 /**
  * Displays all list members and their permissions. Allows adding new members and updating
@@ -53,6 +56,40 @@ export default function MemberSettings({
   roles: Map<string, MemberRole>;
   setMembers: (members: ListMember[]) => unknown;
 }>) {
+  return (
+    <span className='flex flex-col gap-4 shrink overflow-y-auto'>
+      <NewMember
+        listId={listId}
+        members={members}
+        roles={roles}
+        setMembers={setMembers}
+      />
+
+      {members.map(member => (
+        <Member
+          key={member.user.id}
+          listId={listId}
+          member={member}
+          members={members}
+          roles={roles}
+          setMembers={setMembers}
+        />
+      ))}
+    </span>
+  );
+}
+
+function NewMember({
+  roles,
+  listId,
+  members,
+  setMembers
+}: {
+  roles: Map<string, MemberRole>;
+  listId: string;
+  members: ListMember[];
+  setMembers: (members: ListMember[]) => unknown;
+}) {
   const [newMemberEmail, setNewMemberEmail] = useState('');
   const [newRole, setNewRole] = useState(roles.keys().next().value as string);
 
@@ -88,7 +125,56 @@ export default function MemberSettings({
       .catch(addToastForError);
   }
 
-  function handleUpdatePermissions(userId: string, roleId: Selection) {
+  return (
+    <form className='flex gap-2' onSubmit={handleAddMember}>
+      <Input
+        placeholder='Email...'
+        value={newMemberEmail}
+        onValueChange={setNewMemberEmail}
+      />
+      <Select
+        aria-label='New member role'
+        selectedKeys={[newRole]}
+        variant='underlined'
+        onSelectionChange={handleUpdateNewRole}
+      >
+        {Array.from(roles.values())
+          .sort(sortRolesByPermissions)
+          .map(role => (
+            <SelectItem key={role.id} description={role.description}>
+              {role.name}
+            </SelectItem>
+          ))}
+      </Select>
+      <Button
+        className='shrink-0'
+        color='primary'
+        type='submit'
+        variant='ghost'
+      >
+        <PersonPlusFill />
+        Add Member
+      </Button>
+    </form>
+  );
+}
+
+function Member({
+  listId,
+  member,
+  members,
+  roles,
+  setMembers
+}: {
+  listId: string;
+  member: ListMember;
+  members: ListMember[];
+  roles: Map<string, MemberRole>;
+  setMembers: (members: ListMember[]) => unknown;
+}) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  function handleUpdatePermissions(roleId: Selection) {
     // roleId is always `Set<string>`: can't be `"all"` because the `<Select>` is
     // single-select; can't be `Set<number>` because all keys are role IDs, which are
     // strings
@@ -97,7 +183,7 @@ export default function MemberSettings({
     if (!trueRoleId) return; // User tried to clear selection
 
     api
-      .patch(`/list/${listId}/member/${userId}`, { roleId: trueRoleId })
+      .patch(`/list/${listId}/member/${member.user.id}`, { roleId: trueRoleId })
       .then(() => {
         // `trueRoleId` guaranteed to be a key in `roles` because the dropdown options, of
         // which `trueRoleId` is one, is generated based on the `roles`
@@ -105,26 +191,39 @@ export default function MemberSettings({
 
         setMembers(
           members.map(m =>
-            m.user.id === userId ? new ListMember(m.user, role) : m
+            m.user.id === member.user.id ? new ListMember(m.user, role) : m
           )
         );
       })
       .catch(addToastForError);
   }
 
+  function handleRemoveMember() {
+    api
+      .delete(`/list/${listId}/member/${member.user.id}`)
+      .then(() => {
+        setMembers(members.filter(m => m.user.id !== member.user.id));
+      })
+      .catch(addToastForError);
+  }
+
   return (
-    <span className='flex flex-col gap-4 shrink overflow-y-auto'>
-      <form className='flex gap-2' onSubmit={handleAddMember}>
-        <Input
-          placeholder='Email...'
-          value={newMemberEmail}
-          onValueChange={setNewMemberEmail}
+    <>
+      <div key={member.user.id} className='flex gap-4'>
+        <User
+          avatarProps={{
+            classNames: {
+              base: getBackgroundColor(member.user.color)
+            },
+            size: 'sm'
+          }}
+          name={member.user.username ?? member.user.name}
         />
         <Select
-          aria-label='New member role'
-          selectedKeys={[newRole]}
+          aria-label={`${member.user.username ?? member.user.name} Role`}
+          selectedKeys={[member.role.id]}
           variant='underlined'
-          onSelectionChange={handleUpdateNewRole}
+          onSelectionChange={handleUpdatePermissions}
         >
           {Array.from(roles.values())
             .sort(sortRolesByPermissions)
@@ -134,46 +233,19 @@ export default function MemberSettings({
               </SelectItem>
             ))}
         </Select>
-        <Button
-          className='shrink-0'
-          color='primary'
-          type='submit'
-          variant='ghost'
-        >
-          <SendPlus />
-          Send Invite
-        </Button>
-      </form>
-      {members.map(member => (
-        <div key={member.user.id} className='flex gap-4'>
-          <User
-            avatarProps={{
-              classNames: {
-                base: getBackgroundColor(member.user.color)
-              },
-              size: 'sm'
-            }}
-            name={member.user.username ?? member.user.name}
+        <Button isIconOnly color='danger' variant='ghost' onPress={onOpen}>
+          <PersonXFill
+            aria-label={`Remove ${member.user.username ?? member.user.name}`}
           />
-          <Select
-            aria-label={`${member.user.username ?? member.user.name} Role`}
-            selectedKeys={[member.role.id]}
-            variant='underlined'
-            onSelectionChange={handleUpdatePermissions.bind(
-              null,
-              member.user.id
-            )}
-          >
-            {Array.from(roles.values())
-              .sort(sortRolesByPermissions)
-              .map(role => (
-                <SelectItem key={role.id} description={role.description}>
-                  {role.name}
-                </SelectItem>
-              ))}
-          </Select>
-        </div>
-      ))}
-    </span>
+        </Button>
+      </div>
+      <ConfirmModal
+        description='This action will unassign them from all tasks and revoke their access. Cannot be done if this would leave the list without an admin.'
+        isOpen={isOpen}
+        title={`Permanently remove ${member.user.name}?`}
+        onConfirm={handleRemoveMember}
+        onOpenChange={onOpenChange}
+      />
+    </>
   );
 }
