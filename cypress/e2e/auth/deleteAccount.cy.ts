ed9c923e0cae04f625e0606312f1891a714e9cf8
Ryan Grossman <ryangman423@gmail.com>
2026-04-06 16:57:25 -0700

    add wip e2e tests for account deletion

---
+++ cypress/e2e/auth/deleteAccount.cy.ts
@@ -0,0 +1,86 @@
+/**
+ * Tasktix: A powerful and flexible task-tracking tool for all.
+ * Copyright (C) 2025 Nate Baird & other Tasktix contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+describe('delete account', () => {
+  beforeEach(() => {
+    cy.exec('npm run testdb:reset');
+    cy.exec('npm run testdb:seed');
+
+    cy.visit('/');
+  });
+
+  it('Deletion fails if password invalid', () => {
+    cy.login('newUser', 'password123');
+
+    cy.findByLabelText('Profile Actions Dropdown').click();
+    cy.findByLabelText('Profile').should('be.visible').click();
+
+    cy.findByLabelText('Delete Account').click();
+    cy.findByLabelText('Password')
+      .should('be.visible')
+      .type('incorrectpassword');
+
+    cy.findByLabelText('Confirm Delete Account').click();
+
+    cy.get('[data-toast="true"]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Invalid password').should('be.visible');
+      });
+  });
+
+  it.skip('Deletion fails if session is stale', () => {
+    cy.login('newUser', 'password123');
+    cy.clock();
+
+    cy.findByLabelText('Profile Actions Dropdown').click();
+    cy.findByLabelText('Profile').should('be.visible').click();
+
+    cy.findByLabelText('Delete Account').click();
+
+    // Move Clock forward to make session stale
+    cy.tick(65 * 1000);
+
+    cy.findByLabelText('Confirm Delete Account').click();
+
+    cy.get('[data-toast="true"]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Session expired.').should('be.visible');
+      });
+  });
+  it('Deletion succeeds with correct password', () => {
+    cy.login('newUser', 'password123');
+
+    cy.findByLabelText('Profile Actions Dropdown').click();
+    cy.findByLabelText('Profile').should('be.visible').click();
+
+    cy.findByLabelText('Delete Account').click();
+    cy.findByLabelText('Password').should('be.visible').type('password123');
+
+    cy.findByLabelText('Confirm Delete Account').click();
+
+    cy.location('pathname').should('eq', '/about');
+
+    cy.get('[data-toast="true"]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Account succesfully deleted').should('be.visible');
+      });
+  });
+});
